10c104cef9c710c66aba21baa71931fe4e600913
deepss <diewdeep@gmail.com>
2017-06-29 17:23:07 -0700

    add a sample service

--- src/main/groovy/com/startup/apidemo/ApidemoApplication.groovy
+++ src/main/groovy/com/startup/apidemo/ApidemoApplication.groovy
@@ -1,17 +1,48 @@
 package com.startup.apidemo
 
+import com.startup.apidemo.services.ForeignExchangeService
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 @RestController
 @SpringBootApplication
 class ApidemoApplication {
 
-	@RequestMapping("/hello")
-	String home() {
-		return "Hello World!";
+	@RequestMapping('/hello')
+	protected String home() {
+		return 'Hello World!';
+	}
+
+	/*
+	ex- http://localhost:8080/welcome?name=is(deepss)
+	 */
+	@RequestMapping('/welcome')
+	protected String welcomeWithName(
+			@RequestParam('name') String name
+	) {
+		Matcher m = Pattern.compile("\\((.*?)\\)").matcher(name)
+		while(m.find()) {
+			return ('Hello '+m.group(1))
+		}
+		return 'Hi without name'
+	}
+
+	@RequestMapping('/findValue')
+	def whatIsTheValue(
+			@RequestParam('currency') def currency
+	) {
+		try {
+			return ForeignExchangeService.getConversion(currency)
+		}catch (Exception e) {
+			System.out.print(e)
+			return 'Unavailable '
+		}
 	}
 
 	static void main(String[] args) {
---
+++ src/main/groovy/com/startup/apidemo/services/ForeignExchangeService.groovy
@@ -0,0 +1,15 @@
+package com.startup.apidemo.services
+
+import org.springframework.stereotype.Service
+
+/**
+ * Created by dxl0190 on 6/29/17.
+ */
+@Service
+class ForeignExchangeService {
+
+    def getConversion(def fromCurrency) {
+
+        return fromCurrency
+    }
+}
